17b898afe1b3b0aef0a36d86d853ad6f6dbdfbbc
sawyer <superuser@gmail.com>
2019-02-13 08:34:28 +0545

    make ewallet app

---
+++ apps/ewallet/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Here models is a directory and Model is a class. Explore django > db > models in Github
+class Profile(models.Model):
+	MALE = 'm'
+	FEMALE = 'f'
+	GENDER_CHOICES = (
+		(MALE, 'Male'),
+		(FEMALE, 'Female')
+	)
+	profile_pic = models.ImageField(upload_to='profile', null=True, blank=True)
+	address = models.CharField(max_length=30)
+	country = models.CharField(max_length=30)
+	mobile_no = models.CharField(max_length=15, unique=True)
+	dob = models.DateField()
+	gender = models.CharField(max_length=6,
+						choices=GENDER_CHOICES)
+	profession = models.CharField(max_length=20)
+
+	def __str__(self):
+		return self.mobile_no
