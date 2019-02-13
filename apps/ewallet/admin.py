902b0eb510f17fb7d8eb4dc5ae1b3e70545068f6
sawyer <python.craver@gmail.com>
2019-02-14 01:54:37 +0545

    add modelAdminOptions in new class ProfileAdmin with use of decorator in admin.py , overide save method and change timezone to Asia/Kathmandu

--- apps/ewallet/admin.py
+++ apps/ewallet/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from .models import Profile
 
-admin.site.register(Profile)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+	list_display = ('address', 'dob', 'mobile_no', 'country', 'gender', 'profession')
+	list_display_links = ('address','mobile_no') # esma vako editable garna dinu mildaina
+	list_editable = ('dob', 'country')
+	list_filter = ('gender',)
+	search_fields = ['address','mobile_no', 'country']
+# admin.site.register(Profile, ProfileAdmin) # this is old way
