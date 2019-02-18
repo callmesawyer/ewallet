aabb9dcfc6d29e79f561fc58512f8020626b65df
Barsha12 <barshapandey270@gmail.com>
2019-02-18 09:08:58 +0545

    move Profile of models.py and admin.py from ewallet app to user_profile models.py and admin.py respectively

--- apps/ewallet/models.py
+++ apps/ewallet/models.py
@@ -1,25 +1,3 @@
 from django.db import models
 
 # Here models is a directory and Model is a class. Explore django > db > models in Github
-class Profile(models.Model):
-	MALE = 'm'
-	FEMALE = 'f'
-	GENDER_CHOICES = (
-		(MALE, 'Male'),
-		(FEMALE, 'Female')
-	)
-	profile_pic = models.ImageField(upload_to='profile', null=True, blank=True)
-	address = models.CharField(max_length=30)
-	country = models.CharField(max_length=30)
-	mobile_no = models.CharField(max_length=15, unique=True)
-	dob = models.DateField()
-	gender = models.CharField(max_length=6,
-						choices=GENDER_CHOICES)
-	profession = models.CharField(max_length=20)
-
-	def save(self, *args, **kwargs): # override save method
-		self.address = self.address.capitalize()
-		super().save(*args, **kwargs)
-
-	def __str__(self):
-		return self.mobile_no
--- apps/user_profile/admin.py
+++ apps/user_profile/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from datetime import date
 
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+from .models import Profile
+
+
+admin.site.unregister(User)
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline, )
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+
+
+
+
+
+# @admin.register(Profile)
+# class ProfileAdmin(admin.ModelAdmin):
+# 	list_display = ('address', 'dob', 'mobile_no', 'country', 'gender', 'profession', 'get_age')
+# 	list_display_links = ('address','mobile_no') # esma vako editable garna dinu mildaina
+# 	list_editable = ('dob', 'country')
+# 	list_filter = ('gender',)
+# 	search_fields = ['address','mobile_no', 'country']
+
+# 	def get_age(self, instance): # self ma ProfileAdmin ko instance aauxa, instance ma Profile ko instance aauxa
+# 		age = date.today() - instance.dob
+# 		return int(age.days//365.25)
+
+# 	get_age.short_description = 'Age'
+# # admin.site.register(Profile, ProfileAdmin) # this is old way
