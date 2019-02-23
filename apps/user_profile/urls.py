2a556349506d5e22288ad3c57d7c23bf646803f6
sawyer <python.craver@gmail.com>
2019-02-23 21:39:15 +0545

    logout view

--- apps/user_profile/urls.py
+++ apps/user_profile/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 from .views import TransactionView, SignUpView
 
@@ -8,4 +8,5 @@ urlpatterns = [
 	path('signup/', SignUpView.as_view(), name='signup'),
 	path('send/', TransactionView.as_view(), name="send"),
 	path('login/', LoginView.as_view(template_name='login.html'), name='login'), # class based views ma as_view() method call garnu parxas
+	path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- apps/user_profile/views.py
+++ apps/user_profile/views.py
@@ -6,26 +6,6 @@ from django.views.generic import CreateView
 from .models import Transaction, Profile, Account
 from .forms import TransactionForm, CustomUserCreationForm
 
-# def signup(request):
-# 	template_name = "signup.html" 
-# 	if request.method == 'POST':
-# 		signup_form = CustomUserCreationForm(request.POST) # form with data
-# 		if signup_form.is_valid():
-# 			user = signup_form.save()
-
-# 			profile = Profile.objects.create(user=user)
-# 			account = Account.objects.create(user=user)
-# 			profile.mobile = signup_form.cleaned_data['mobile']
-# 			profile.save()
-# 			account.save()
-
-# 			return redirect('home')
-# 	else:
-# 		signup_form = CustomUserCreationForm() #unbounded form
-# 	context = {
-# 		'form': signup_form,
-# 	}
-# 	return render(request, template_name, context)
 
 class SignUpView(CreateView):
 	form_class = CustomUserCreationForm
